1919a508ea60b3d360ef8b2cf9f6af185de9777f
Wazard <34705162+Wazard@users.noreply.github.com>
2025-03-05 15:13:43 +0100

    conveyor and phone
    socket and moving belts

---
+++ Library/VP/mppm6dc924a1/Assets/Scripts/ConveyorBelt.cs
@@ -0,0 +1,43 @@
+using System;
+using Unity.Mathematics;
+using Unity.Netcode;
+using UnityEngine;
+using XRMultiplayer;
+
+namespace MultiUserU6
+{
+    /// <summary>
+    /// Physically accurate conveyor belt. Uses physics in order to move everything that touches it.
+    /// </summary>
+public class ConveyorBelt : MonoBehaviour
+{
+    [SerializeField, Tooltip("Speed of which items on top are moved"), Range(0.01f,2)]
+    private float _beltSpeed = .5f;
+    [SerializeField,Tooltip("Conveyor belt status")]
+    private bool _isActive = true;
+    private Rigidbody _rigidBody;
+    private NetworkManagerVRMultiplayer _networkManager;
+
+    void OnEnable()
+    {
+        _networkManager = FindFirstObjectByType<NetworkManagerVRMultiplayer>();
+    }
+        void Start()
+    {
+        _networkManager.OnConnectionEvent += (NetworkManager, ConnectionEventData) => {_rigidBody.isKinematic=true;};
+        _rigidBody ??= GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if(!_isActive)
+            return;
+
+        Vector3 currentPosition = _rigidBody.position;
+        
+        _rigidBody.position += transform.forward * _beltSpeed * Time.fixedDeltaTime;
+        _rigidBody.MovePosition(currentPosition);
+    }
+
+}
+}
